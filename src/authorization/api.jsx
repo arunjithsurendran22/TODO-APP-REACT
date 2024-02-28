290450392ecf9a80f6694d182a8fc5a7182367c5
Arunjith Surendran <arunjithsurendran22@gmail.com>
2024-02-29 00:23:28 +0530

    final

--- src/authorization/api.jsx
+++ src/authorization/api.jsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v2/todo"
+  baseURL: "https://todo-app-react-server.vercel.app/api/v2/todo"
 });
+//http://localhost:3000/api/v2/todo
+//https://todo-app-react-server.vercel.app/
 // Request interceptor for adding the bearer token
 api.interceptors.request.use(
   (config) => {
